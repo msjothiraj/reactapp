e333a6a667ae59560bc1b5f5d8c9f864505bd781
msjothiraj@gmail.com <msjothiraj@gmail.com>
2019-08-17 18:02:42 +0530

    Updates

---
+++ precache-manifest.c6eafed94a6926b69fe817f1d7973544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378cd2f51a93e2f1ff54bad36ad76440",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "b8ca989080d2532494b4",
+    "url": "/reactapp/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "cda38382abf9a879a76a",
+    "url": "/reactapp/static/css/main.e2371ae6.chunk.css"
+  },
+  {
+    "revision": "b8ca989080d2532494b4",
+    "url": "/reactapp/static/js/2.7431f0ac.chunk.js"
+  },
+  {
+    "revision": "cda38382abf9a879a76a",
+    "url": "/reactapp/static/js/main.cb6a1481.chunk.js"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
